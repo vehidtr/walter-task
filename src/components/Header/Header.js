71b43254b7da8b4e0e9f9be3d922d3ee3feda7ac
Vehid Trtak <vehit.trtak@gmail.com>
2020-09-28 00:41:36 +0200

    Header component

---
+++ src/components/Header/Header.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css';
+
+export const Header = ({ search, setSearch }) => {
+  return (
+    <header className='Header-header'>
+      <div className='Header-logo'>
+        <Link to='/'>
+          <img src='/images/logo.png' alt='Flash news' />
+        </Link>
+      </div>
+      <input
+        className='Header-search-field'
+        type='text'
+        placeholder='Type to search..'
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
+    </header>
+  );
+};
